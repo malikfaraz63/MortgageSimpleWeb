90bd6bda10b8b035c6ee98e5e276e0bb75a90cdb
Faraz Malik <malikfaraz63@gmail.com>
2023-07-05 23:52:37 +0100

    Added in-session refresh

--- public/app.js
+++ public/app.js
@@ -2,12 +2,12 @@ const auth = firebase.auth();
 const db = firebase.firestore();
 let userSignedIn = false;
 
-const monthNames = [ "January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December" ];
+const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"];
 
 /// Fetches mortgage stats by loan amount
 function fetchMortgageStats() {
-    return new Promise(async function(resolve, reject) {
+    return new Promise(async function (resolve, reject) {
         var mortgagesData = [];
         const loanStep = 200000;
         const slices = 5;
@@ -24,7 +24,7 @@ function fetchMortgageStats() {
                 });
         }
         const finalCountQuery = db.collectionGroup('mortgages')
-                .where('loan', '>=', loanStep * slices);
+            .where('loan', '>=', loanStep * slices);
         finalCountQuery.get()
             .then(querySnapshot => {
                 mortgagesData.splice(slices, 0, querySnapshot.size);
@@ -34,7 +34,7 @@ function fetchMortgageStats() {
 }
 
 function fetchRecentLeads() {
-    return new Promise(async function(resolve,reject) {
+    return new Promise(async function (resolve, reject) {
         const recentMortgages = db.collectionGroup("mortgages")
             .orderBy("start", "desc")
             .limit(10); // MARK: Make variable
@@ -51,36 +51,37 @@ function fetchRecentLeads() {
     });
 }
 
+let myChart;
 function showMortgageStats(mortgagesData) {
     'use strict'
     console.log(mortgagesData);
     // Graphs
     const ctx = document.getElementById('myChart')
     // eslint-disable-next-line no-unused-vars
-    const myChart = new Chart(ctx, {
-      type: 'pie',
-      data: {
-          labels: [
-              '£0-£200k',
-              '£200k-£400k',
-              '£400k-£600k',
-              '£600k-£800k',
-              '£800k-£1m',
-              '>£1m'
-          ],
-          datasets: [{
-              data: mortgagesData,
-              backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
-              hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"],
-          }]
-      },
-      options: {
-        plugins: {
-          tooltip: {
-                boxPadding: 3
-          }
+    myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: [
+                '£0-£200k',
+                '£200k-£400k',
+                '£400k-£600k',
+                '£600k-£800k',
+                '£800k-£1m',
+                '>£1m'
+            ],
+            datasets: [{
+                data: mortgagesData,
+                backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+                hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"],
+            }]
+        },
+        options: {
+            plugins: {
+                tooltip: {
+                    boxPadding: 3
+                }
+            }
         }
-      }
     })
 }
 
@@ -110,7 +111,7 @@ function extractUserId(documentID) {
 }
 
 function fetchUser(userID) {
-    return new Promise(async function(resolve, reject) {
+    return new Promise(async function (resolve, reject) {
         let userQuery = db.collection("users").doc(userID);
         userQuery.get()
             .then(user => {
@@ -174,39 +175,58 @@ var provider = new firebase.auth.GoogleAuthProvider();
 
 const menuList = document.getElementById("menuList");
 const dashboardView = document.getElementById("dashboardView");
+const button = document.getElementById("signInButton");
+const reloadDataButton = document.getElementById("reloadDataButton");
 dashboardView.hidden = true;
 
-const button = document.getElementById("signInButton");
+function clearData() {
+    document.getElementById("leadsTable").innerHTML = "";
+    if (myChart) {
+        myChart.destroy();
+    }
+    for (let i = 0; i < menuItems.length; i++) {
+        menuList.removeChild(menuList.children[0]);
+    }
+}
+
+function reloadData() {
+    fetchMortgageStats()
+        .then(mortgagesData => {
+            dashboardView.hidden = false;
+            button.className = "btn btn-link align-items-center gap-2";
+            button.lastChild.data = " Sign out";
+            for (let i = menuItems.length - 1; i >= 0; i--) {
+                menuList.insertAdjacentElement('afterbegin', getMenuItem(menuItems[i].title, menuItems[i].xLink));
+            }
+            showMortgageStats(mortgagesData);
+        });
+
+    fetchRecentLeads()
+        .then(showRecentLeads);
+}
+
+reloadDataButton.addEventListener('click', () => { 
+    clearData(); 
+    reloadData(); 
+});
+
 button.addEventListener('click', () => {
     if (userSignedIn) {
         auth.signOut().then(() => {
             dashboardView.hidden = true;
             button.lastChild.data = " Sign in";
-            for (let i = 0; i < menuItems.length; i++) {
-                menuList.removeChild(menuList.children[0]);
-            }
             button.className = "btn btn-link align-items-center gap-2";
         });
         userSignedIn = false;
         button.className = "btn btn-link align-items-center gap-2 disabled";
-        document.getElementById("leadsTable").innerHTML = "";
+        reloadDataButton.className = "btn btn-link align-items-center gap-2 disabled";
+        clearData();
     } else {
         auth.signInWithPopup(provider)
-        .then((result) => {
-            fetchMortgageStats()
-                .then( mortgagesData => {
-                    dashboardView.hidden = false;
-                    button.className = "btn btn-link align-items-center gap-2";
-                    button.lastChild.data = " Sign out";
-                    for (let i = menuItems.length - 1; i >= 0; i--) {
-                        menuList.insertAdjacentElement('afterbegin', getMenuItem(menuItems[i].title, menuItems[i].xLink));
-                    }
-                    showMortgageStats(mortgagesData);
-                });
-            
-            fetchRecentLeads()
-                .then(showRecentLeads);
-        });
+            .then((result) => {
+                reloadDataButton.className = "btn btn-link align-items-center gap-2";
+                reloadData();
+            });
         userSignedIn = true;
         button.className = "btn btn-link align-items-center gap-2 disabled";
     }
